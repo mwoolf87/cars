c6a3d16e695e4f513b302f6db1f02ce4e0be817a
mwoolf87 <mwoolfdev@gmail.com>
2023-03-26 21:14:53 -0400

    adjusted carsSlice key car to go from car to data

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CarList = () => {
+  const cars = useSelector(state => {
+    return state.cars.data;
+  });
   return <div>CarList</div>;
 };
 
