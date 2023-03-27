19f07c6cbe70822628442f3e3b326911b38d1fff
mwoolf87 <mwoolfdev@gmail.com>
2023-03-26 22:14:15 -0400

    added logic to style a car if name is included in name form submission

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,10 +5,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
 const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter(car =>
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter(car =>
       car.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+    return {
+      cars: filteredCars,
+      name: form.name
+    };
   });
 
   const handleCarDelete = car => {
@@ -16,8 +20,9 @@ const CarList = () => {
   };
 
   const renderedCars = cars.map(car => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
